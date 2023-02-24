3003bda31a96895379fddce60fb0e3e1040ee5df
David Uzondu <uzondudavid2004@gmail.com>
2023-02-24 15:00:04 +0100

    Made a minor change

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -18,7 +18,7 @@ function PostList({ news, page, setPage, setClicked, type }) {
         return (
             <div className='postlist md:w-2/3 md:m-auto w-full pt-5 flex flex-col' id='scrollbar1'>
                 <div className='md:text-lg text-sm justify-center md:font-normal flex flex-wrap items-center mt-2 bg-blue-200 rounded-full mb-3 w-fit m-auto px-3'>
-                    Made with ❤️ by
+                    Made with ❤️ and ☕ by
                     <a href={"https://www.twitter.com/daveuzondu"} className="ml-2 mr-2 underline">David Uzondu </a> -
                     <a href='https://github.com/daviduzondu/lotusreader' className="ml-2 mr-2 underline" > <BsGithub className='inline mr-2' />Star on Github</a> -
                     <a href='#' className="ml-2 underline">  <AiOutlineRead className='inline mr-2' />Read the Dev.to post</a>
